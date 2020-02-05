2cbc0a528d31156ce46d81648b9f00dcc8a72569
rnyewale <rnyewale24@gmail.com>
2020-02-05 12:02:12 +0530

    feature-1 : pushing changes to master.

--- src/SampleClass.java
+++ src/SampleClass.java
@@ -4,7 +4,10 @@ public class SampleClass {
 	public static void main(String[] args) {
 		String username="rnyewale";
 		int rollNo = 2411;
+		String address ="Pune Maharashtra";
+		
 		System.out.println("GIT Congig Success :- "+username+"\t roll No: "+rollNo);
+		System.out.println("Address : "+address);
 		
 	}
 
